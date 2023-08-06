2ab92d9843f532ec05c3361d3babc0ae7502346c
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-08-06 17:13:26 +1000

    Shared xpos for sprites

--- core/src/com/migh/game/animation/michael/Michael.kt
+++ core/src/com/migh/game/animation/michael/Michael.kt
@@ -3,19 +3,30 @@ package com.migh.game.animation.michael
 import com.badlogic.gdx.Input
 
 class Michael {
-    private val walk = MichaelWalk(1, 3, 50, 50, "M/M_walk.png")
-    private val jump = MichaelJump(1, 3, 50, 50, "M/M_jump.png")
+    private val walk = MichaelWalk(1, 4, 50, 50, "M/M_walk.png")
+    private val jump = MichaelJump(1, 5, 50, 50, "M/M_jump.png")
 
-    // Need to pass current x value between actions
-
+    private var xPosition: Int = walk.x
+    // Need to allow jump animation to play more than once?
+    // Need backwards jump action
+    // Also need to sort disappearing sprite -> Need to just render it
     fun render(keycode: Int) {
         when (keycode) {
-            in Input.Keys.LEFT..Input.Keys.RIGHT -> walk.render(keycode)
-            Input.Keys.SPACE -> jump.render(keycode)
+            in Input.Keys.LEFT..Input.Keys.RIGHT -> {
+                walk.x = xPosition
+                walk.render(keycode)
+                xPosition = walk.x
+            }
+            Input.Keys.SPACE -> {
+                jump.x = xPosition
+                jump.render(keycode)
+                xPosition = jump.x
+            }
         }
     }
 
     fun dispose() {
         walk.dispose()
+        jump.dispose()
     }
 }
